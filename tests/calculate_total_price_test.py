524a362a0b98ec2fdf47d6c1f173a9c69e53d449
Calvin <cod11@georgetown.edu>
2020-03-24 19:03:54 -0400

    fixed test to be correct

--- tests/calculate_total_price_test.py
+++ tests/calculate_total_price_test.py
@@ -4,4 +4,4 @@ basket = ["1", "5", "7", "3"]
 
 def test_calculate_total_price_subtotal():
     result = calculate_total_price(basket)["subtotal"]
-    assert result == 0.00
+    assert result == 17.48
