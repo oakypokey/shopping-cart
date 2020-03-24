fde6ffe59dbe01698b16ba6598ad117cb4fd8597
Calvin <cod11@georgetown.edu>
2020-03-24 19:09:34 -0400

    added calculate total price tests

--- tests/calculate_total_price_test.py
+++ tests/calculate_total_price_test.py
@@ -1,7 +1,29 @@
 from shopping_cart import calculate_total_price
 
-basket = ["1", "5", "7", "3"]
+basket1 = ["1", "5", "7", "3"]
 
-def test_calculate_total_price_subtotal():
-    result = calculate_total_price(basket)["subtotal"]
+def test_calculate_total_price_basket1_subtotal():
+    result = calculate_total_price(basket1)["subtotal"]
     assert result == 17.48
+
+def test_calculate_total_price_basket1_tax():
+    result = calculate_total_price(basket1)["tax"]
+    assert result == 1.5294999999999999
+
+def test_calculate_total_price_basket1_total():
+    result = calculate_total_price(basket1)["total"]
+    assert result == 19.009500000000003
+
+basket2 = ["5", "10", "3", "3"]
+
+def test_calculate_total_price_basket2_subtotal():
+    result = calculate_total_price(basket2)["subtotal"]
+    assert result == 15.96
+
+def test_calculate_total_price_basket2_tax():
+    result = calculate_total_price(basket2)["tax"]
+    assert result == 1.3965
+
+def test_calculate_total_price_basket2_total():
+    result = calculate_total_price(basket2)["total"]
+    assert result == 17.3565
