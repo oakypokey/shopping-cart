b7ace27a328bbe3d3bd2979271ddde3f930127d8
Calvin <cod11@georgetown.edu>
2020-03-24 18:22:48 -0400

    added invalid argument test and cleaned up other tests

--- tests/find_product_test.py
+++ tests/find_product_test.py
@@ -1,15 +1,22 @@
 from shopping_cart import find_product
 
-def test_find_product_valid():
+def test_find_product_validID():
     try:
         result = find_product("1")
         assert result.serial == "1" and result.name == "Chocolate Sandwich Cookies"
     except:
         assert False
 
-def test_find_product_invalid():
+def test_find_product_invalidID():
     try:
-        result = find_product("40")
+        find_product("40")
+        assert False
+    except:
+        assert True
+
+def test_find_product_typeNumber():
+    try:
+        find_product(1)
         assert False
     except:
         assert True
