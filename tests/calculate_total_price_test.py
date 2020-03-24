4adfcad7a6b0ecb6638d21c35f1a79f62bc11dca
Calvin <cod11@georgetown.edu>
2020-03-24 19:02:59 -0400

    test fix

--- tests/calculate_total_price_test.py
+++ tests/calculate_total_price_test.py
@@ -2,6 +2,6 @@ from shopping_cart import calculate_total_price
 
 basket = ["1", "5", "7", "3"]
 
-def calculate_total_price_subtotal_test():
+def test_calculate_total_price_subtotal():
     result = calculate_total_price(basket)["subtotal"]
     assert result == 0.00
